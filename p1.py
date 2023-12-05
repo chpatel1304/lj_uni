8cc7c583e49fadc6a3728f0c0f842618398aaa42
chpatel1304 <chpatel1304@gmail.com>
2023-12-05 13:07:02 +0530

    DONE

--- p1.py
+++
@@ -1,9 +0,0 @@
-r = 5
-for i in range(1, r+1):
-    print("*"*i, end="")
-    print(" "*(r-i)*2, end="")
-    print("*"*i)
-for i in range(r,0,-1):
-    print("*"*i, end="")
-    print(" "*(r-i)*2, end="")
-    print("*"*i)    
