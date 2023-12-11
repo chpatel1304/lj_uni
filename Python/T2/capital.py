3f845d16d39827a46c604e084b581779f6f146f1
chpatel1304 <chpatel1304@gmail.com>
2023-12-11 13:17:47 +0530

    Done

---
+++ Python/T2/capital.py
@@ -0,0 +1,8 @@
+#wap to create a list of words of string entered by user each word
+#has first and last letter capital
+s="abcd efgh pqr"
+n_l=[]
+l=s.split()
+for i in l:
+    n_l.append(i[0].upper()+i[1:-1:1]+i[-1].upper())
+print(n_l)
---
+++ Python/T2/runningsum.py
@@ -0,0 +1,7 @@
+l=[1,2,3,4,5,6]
+l1=[]
+for i in range(len(l)-1):
+    sum=l[i]+l[i+1]
+    l1.append(sum)
+print(l)
+print(l1)
